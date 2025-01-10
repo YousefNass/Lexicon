66e0ff239ab59a801df054fd99f5ef15b21e5949
karim <rematchlop@gmail.com>
2025-01-10 13:25:59 +0000

    added jenkins script

---
+++ run-jenkins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git add .
+git commit -m "starting jenkins"]
+git push origin main
+
+JENKINS_URL=""
+JENKINS_JOB=""
+JENKINS_TOKEN=""
+
+curl -X POST "http://${JENKINS_URL}/job/${JENKINS_JOB}/build?token=${JENKINS_TOKEN}"
