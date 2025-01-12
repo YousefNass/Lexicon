258e5fecc796def6338d04264b6a132011d1098e
karim <rematchlop@gmail.com>
2025-01-12 13:42:18 +0200

    starting jenkins

--- run-jenkins.sh
+++ run-jenkins.sh
@@ -6,6 +6,7 @@ git push origin main
 
 JENKINS_URL="http://localhost:8080/"
 JENKINS_JOB="lexicon-job"
-JENKINS_TOKEN="11148fe58bea485bec553fa283c049a271"
+JENKINS_TOKEN="testAuth"
+JENKINS_USER="weezo"
 
-curl -u "weezo:11148fe58bea485bec553fa283c049a271" -X POST "${JENKINS_URL}/job/${JENKINS_JOB}/build?token=${JENKINS_TOKEN}"
+curl -u "${JENKINS_USER}:${JENKINS_TOKEN}" -X POST "${JENKINS_URL}/job/${JENKINS_JOB}/build?token=${JENKINS_TOKEN}"
