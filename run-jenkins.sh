27a49c4e8214b734d29ee821df2cba9f0a7881f2
yousef <yousefnasser0002@gmail.com>
2025-01-17 11:42:17 +0000

    starting jenkins

--- run-jenkins.sh
+++ run-jenkins.sh
@@ -4,6 +4,6 @@ git add .
 git commit -m "starting jenkins"
 git push origin main
 
-JENKINS_URL="http://localhost:8080/"
+JENKINS_URL="http://192.168.64.6:8080/"
 
 curl -X POST "${JENKINS_URL}/buildByToken/build?job=lexicon-job&token=lexical-auth"
